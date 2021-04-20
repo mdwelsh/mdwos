0e2f648893e2bfad3230d2a17c0f95080ce4a9b5
Matt Welsh <mdw@mdw.la>
2021-04-19 19:56:05 -0700

    Working on this

--- img2hgr/img2hgr.py
+++ img2hgr/img2hgr.py
@@ -11,10 +11,10 @@ BYTES_PER_COL = 8
 
 
 def a2convert(r: int, g: int, b: int) -> int:
-    if (r == 0 and g == 0 and b == 0):
+    if r == 0 and g == 0 and b == 0:
         return 0
     else:
-        return 7
+        return 1
 
 
 def process_image(infile, outfile, name, width, height):
@@ -26,6 +26,8 @@ def process_image(infile, outfile, name, width, height):
     pixels = list(img.getdata())
     print(f"MDW: THERE ARE {len(pixels)} pixels")
 
+    curpixel = 0
+    shift = 7
 
     with open(outfile, "w+") as outf:
         outf.write(
@@ -36,16 +38,23 @@ def process_image(infile, outfile, name, width, height):
         outf.write(f"{name}:\n")
         for pixel in pixels:
             r, g, b = pixel
-            a2pixel = a2convert(r, g, b)
-            if col == 0:
-                outf.write("  .byte ")
-            outf.write(f"${a2pixel:02x}")
-            col += 1
-            if col < BYTES_PER_COL:
-                outf.write(",")
-            else:
-                outf.write("\n")
-                col = 0
+            if r >= 128 or g >= 128 or b >= 128:
+                curpixel = curpixel | (1 << shift-1)
+
+            shift = shift - 1
+            if shift == 0:
+                # Finished with output byte
+                if col == 0:
+                    outf.write("  .byte ")
+                outf.write(f"${curpixel:02x}")
+                col += 1
+                if col < BYTES_PER_COL:
+                    outf.write(",")
+                else:
+                    outf.write("\n")
+                    col = 0
+                shift = 7
+                curpixel = 0
 
         outf.write("\n")
     print(f"Saved assembly to {outfile}")

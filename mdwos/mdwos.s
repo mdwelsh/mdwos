64591889b8716c8cc6d1d7e0fc72e86b906b7cd6
Matt Welsh <mdw@mdw.la>
2021-04-25 16:58:48 -0700

    Menu functions mostly work

--- mdwos/mdwos.s
+++ mdwos/mdwos.s
@@ -10,7 +10,8 @@
 .segment "STARTUP"
 
 PRINTPTR = $CE  ; Address used by print function
-LINKPTR = $EB   ; Address used by showlink function
+LINKPTR = $CE   ; Address used by showlink function
+LINKJUMP = $ED  ; Address used by dolinks
 
 ; Apple II constants
 TEXTMODE = $32
@@ -53,6 +54,7 @@ main_menu:
   STA XCURSOR
   JSR print
   .ASCIIZ "MDW OS v0.1.1"
+  .byte 0
 
   LDA #NEWLINE
   JSR COUT
@@ -60,6 +62,7 @@ main_menu:
   STA XCURSOR
   JSR print
   .ASCIIZ "(c) 1985 by Matt Welsh"
+  .byte 0
 
   LDA #NEWLINE
   JSR COUT
@@ -68,136 +71,28 @@ main_menu:
   STA XCURSOR
   JSR print
   .ASCIIZ "Please select one:"
+  .ASCIIZ " "
+  .byte 0
 
-  LDA #NEWLINE
-  JSR COUT
-  JSR COUT
-  LDA #6
-  STA XCURSOR
-  JSR print
-  .ASCIIZ "1. About Matt"
-
-  LDA #NEWLINE
-  JSR COUT
-  JSR COUT
-  LDA #6
-  STA XCURSOR
-  JSR print
-  .ASCIIZ "2. Research papers and talks"
-
-  LDA #NEWLINE
-  JSR COUT
-  JSR COUT
-  LDA #6
-  STA XCURSOR
-  JSR print
-  .ASCIIZ "3. Resume and CV"
-
-  LDA #NEWLINE
-  JSR COUT
-  JSR COUT
-  LDA #6
-  STA XCURSOR
-  JSR print
-  .ASCIIZ "4. About this website"
+  ; Store address of our links in LINKPTR
+  LDA #<main_menu_links
+  STA LINKPTR
+  LDA #>main_menu_links
+  STA LINKPTR+1
+  JSR showlinks
 
   LDA #NEWLINE ; newline
   JSR COUT
-  JSR COUT
-  JSR COUT
-  LDA #1
-  STA XCURSOR
-  LDA #NEWLINE ; newline
-  JSR COUT
   JSR print
-  .ASCIIZ " < Click on selection or type 1-4 >"
+  .ASCIIZ " < Click on selection or type 1-5 >"
+  .byte 0
 
-@main_menu_loop:
-  ; Read paddle 0 button
-  BIT PBTN0
-  BPL @main_menu_key
-  ; Otherwise, button was clicked.
-
-  LDA #4
-  STA XCURSOR
-  STA YCURSOR
-
-  ; Read paddle 1 (y-position)
-  LDX #1
-  JSR PREAD
-  TYA
-  JSR PRBYTE
-
-  CPY #$32  ; Lowest y-position of link
-  BCC @check_link2
-  CPY #$3B  ; Highest y-position of link
-  BCS @check_link2
-  JSR about_matt
-  JMP main_menu
-
-@check_link2:
-  CPY #$47  ; Lowest y-position of link
-  BCC @check_link3
-  CPY #$50  ; Highest y-position of link
-  BCS @check_link3
-  JSR pubs
-  JMP main_menu
-
-@check_link3:
-  CPY #$5A  ; Lowest y-position of link
-  BCC @check_link4
-  CPY #$64  ; Highest y-position of link
-  BCS @check_link4
-  JSR resume
-  JMP main_menu
-
-@check_link4:
-  CPY #$70  ; Lowest y-position of link
-  BCC @main_menu_key
-  CPY #$79  ; Highest y-position of link
-  BCS @main_menu_key
-  JSR about_website
-  JMP main_menu
-
-@main_menu_key:
-  ; Check for keypress
-  LDA $C000
-  BPL @main_menu_loop
-  STA $C010
-  JSR COUT ; echo out
-
-@menu_1:
-  CMP #'1'|$80
-  BNE @menu_2
-  JSR about_matt
-  JMP main_menu
-
-@menu_2:
-  CMP #'2'|$80
-  BNE @menu_3
-  JSR pubs
-  JMP main_menu
-
-@menu_3:
-  CMP #'3'|$80
-  BNE @menu_4
-  JSR resume
-  JMP main_menu
-
-@menu_4:
-  CMP #'4'|$80
-  BNE @menu_bad
-  JSR about_website
-  JMP main_menu
-
-@menu_bad:
-  LDA #NEWLINE ; newline
-  JSR COUT
-  LDA #1
-  STA XCURSOR
-  JSR print
-  .ASCIIZ "Bad input!"
-  JMP @main_menu_loop
+  ; Check for input
+  LDA #<main_menu_links
+  STA LINKPTR
+  LDA #>main_menu_links
+  STA LINKPTR+1
+  JMP dolinks
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; About Matt page
@@ -211,6 +106,7 @@ about_matt:
   JSR COUT
   JSR print
   .ASCIIZ "BLOAD MDWIMG,A$4000"
+  .byte 0
   LDA #NEWLINE
   JSR COUT
 
@@ -219,13 +115,13 @@ about_matt:
   BPL @about_matt_key
   ; Otherwise, button was clicked.
   JSR TEXT
-  RTS
+  JMP main_menu
 @about_matt_key:
   ; Check for keypress
   LDA $C000
   BPL @about_matt_loop
   STA $C010
-  RTS
+  JMP main_menu
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Publications and talks page
@@ -237,6 +133,7 @@ pubs:
   STA XCURSOR
   JSR print
   .ASCIIZ "Matt Welsh - Publications and talks"
+  .byte 0
 
   LDA #NEWLINE
   JSR COUT
@@ -244,6 +141,7 @@ pubs:
   STA XCURSOR
   JSR print
   .ASCIIZ "-----------------------------------"
+  .byte 0
 
   LDA #NEWLINE
   JSR COUT
@@ -251,11 +149,13 @@ pubs:
 
   JSR print
   .ASCIIZ "Would you like me to open a link to"
+  .byte 0
   LDA #NEWLINE
   JSR COUT
 
   JSR print
   .ASCIIZ "my research papers page (Y/N)?"
+  .byte 0
 
 @pubs_input:
   JSR KEYIN
@@ -278,6 +178,7 @@ pubs:
   STA XCURSOR
   JSR print
   .ASCIIZ "-- Press any key to go back --"
+  .byte 0
   JSR KEYIN
   RTS
 
@@ -288,6 +189,7 @@ pubs:
   STA XCURSOR
   JSR print
   .ASCIIZ "Bad input! Please type Y or N."
+  .byte 0
   JMP @pubs_input
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -300,6 +202,7 @@ resume:
   STA XCURSOR
   JSR print
   .ASCIIZ "Matt Welsh - Resume and CV"
+  .byte 0
 
   LDA #NEWLINE
   JSR COUT
@@ -307,6 +210,7 @@ resume:
   STA XCURSOR
   JSR print
   .ASCIIZ "--------------------------"
+  .byte 0
 
   LDA #NEWLINE
   JSR COUT
@@ -314,11 +218,13 @@ resume:
 
   JSR print
   .ASCIIZ "Would you like me to open a link to"
+  .byte 0
   LDA #NEWLINE
   JSR COUT
 
   JSR print
   .ASCIIZ "my resume (Y/N)?"
+  .byte 0
 
 @resume_input:
   JSR KEYIN
@@ -343,6 +249,7 @@ resume:
   STA XCURSOR
   JSR print
   .ASCIIZ "Bad input! Please type Y or N."
+  .byte 0
   JMP @resume_input
 
 @ask_cv:
@@ -351,11 +258,13 @@ resume:
   JSR COUT
   JSR print
   .ASCIIZ "Would you like me to open a link to"
+  .byte 0
   LDA #NEWLINE
   JSR COUT
 
   JSR print
   .ASCIIZ "my full CV (Y/N)?"
+  .byte 0
 
 @cv_input:
   JSR KEYIN
@@ -378,6 +287,7 @@ resume:
   STA XCURSOR
   JSR print
   .ASCIIZ "-- Press any key to go back --"
+  .byte 0
   JSR KEYIN
   RTS
 
@@ -388,6 +298,7 @@ resume:
   STA XCURSOR
   JSR print
   .ASCIIZ "Bad input! Please type Y or N."
+  .byte 0
   JMP @cv_input
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -395,21 +306,20 @@ resume:
 
 about_website:
   JSR HOME
-  LDA #4
-  STA XCURSOR
   JSR print
   .ASCIIZ "About this website"
-
-  LDA #NEWLINE
-  JSR COUT
-  LDA #4
-  STA XCURSOR
-  JSR print
   .ASCIIZ "------------------"
+  .ASCIIZ " "
+  .ASCIIZ " "
+  .byte 0
 
-  LDA #NEWLINE
-  JSR COUT
-  JSR COUT
+  JSR print
+  .ASCIIZ "This site was built in hand-coded 6502"
+  .ASCIIZ "assembly language, based on AppleIIjs,"
+  .ASCIIZ "a JavaScript-based Apple ][ emulator."
+  .ASCIIZ "For more info, check out these links!"
+  .ASCIIZ " "
+  .byte 0
 
   ; Store address of our links in LINKPTR
   LDA #<about_links
@@ -418,41 +328,12 @@ about_website:
   STA LINKPTR+1
   JSR showlinks
 
-@about_loop:
-  ; Read paddle 0 button
-  BIT PBTN0
-  BPL @about_key
-  ; Otherwise, button was clicked.
-
-  ; Read paddle 1 (y-position)
-  LDX #1
-  JSR PREAD
-
-  CPY #$65  ; Lowest y-position of first link
-  BCC @check_link2
-  CPY #$6F  ; Highest y-position of first link
-  BCS @check_link2
-  ; Now we know we are on the link.
-  JSR doopenlink
-  .ASCIIZ "https://github.com/mdwelsh/mdwos-6502"
-  RTS
-
-@check_link2:
-  CPY #$7A  ; Lowest y-position of second link
-  BCC @about_key
-  CPY #$84  ; Highest y-position of second link
-  BCS @about_key
-  ; Now we know we are on the link.
-  JSR doopenlink
-  .ASCIIZ "https://www.scullinsteel.com/apple2/"
-  RTS
-
-@about_key:
-  ; Check for keypress
-  LDA $C000
-  BPL @about_loop
-  STA $C010
-  RTS
+  ; Check for input
+  LDA #<about_links
+  STA LINKPTR
+  LDA #>about_links
+  STA LINKPTR+1
+  JMP dolinks
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Utilities
@@ -461,7 +342,10 @@ about_website:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; print
 ;;
-;; Prints the ASCIIZ string following the `jsr print` instruction to the screen.
+;; Prints ASCIIZ strings following the `jsr print` instruction to the screen,
+;; with a newline in between each line. If the string starts with a zero byte
+;; it is the last string in the sequence.
+
 print:
   ; On entry, the return address-1 is on the stack.
   ; This will be the first byte of the string we want to print.
@@ -472,27 +356,41 @@ print:
   PLA
   STA PRINTPTR+1
 
-  LDY #0
-@printloop:
-  ; We inc here because the address we pulled off the stack was
-  ; the return address-1.
+  ; The address we pulled off the stack was the return
+  ; address-1.
   INC PRINTPTR
-  BNE @noc2  ; If no carry
+  BNE @print_line
   INC PRINTPTR+1
-@noc2:
+
+@print_line:
   ; Load the next character
+  LDY #0
   LDA (PRINTPTR),Y
   ; If null, stop printing.
-  BEQ @printend
-  ; Print it out.
+  BEQ @print_line_end
+  ; Print char
   ORA #$80
-  JSR COUT ; cout
+  JSR COUT
+  ; Increment the pointer
+  INC PRINTPTR
+  BNE @print_noc
+  INC PRINTPTR+1
+@print_noc:
+  JMP @print_line
 
-;  LDA #$10 ; wait amount
-;  JSR WAIT
-
-  JMP @printloop
-@printend:
+@print_line_end:
+  ; Newline
+  LDA #NEWLINE
+  JSR COUT
+  ; Check to see if there is another line to print.
+  INC PRINTPTR
+  BNE @print_end_noc
+  INC PRINTPTR+1
+@print_end_noc:
+  LDY #0
+  LDA (PRINTPTR),Y
+  ; If non-null, print next line.
+  BNE @print_line
   ; Now, we take the current pointer into the string we were printing
   ; and push it back on the stack as the return address.
   ; This will resume execution after the string.
@@ -545,32 +443,20 @@ doopenlink:
   PHA
 
   ; Finally, jump to the link.
+  ; Obviously this won't work on a non-MDWOS enabled machine.
   LDA OPENLINK
-
   RTS
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; showlinks
+;; Show a list of links, each with a corresponding key shortcut.
 ;; 
 ;; LINKPTR contains first link of a page, with format as described below.
-
 showlinks:
-
 @shownextlink:
   LDY #0
   LDA (LINKPTR),Y  ; Index of link
   BEQ @lastlink
-
-  LDA #'['|$80
-  JSR COUT
-  LDY #0           ; Get link index again
-  LDA (LINKPTR),Y
-  ORA #$80
-  JSR COUT
-  LDA #']'|$80
-  JSR COUT
-  LDA #' '|$80
-  JSR COUT
 
   ; Advance LINKPTR by 65bytes so we get the first byte of the link text.
   INC LINKPTR
@@ -617,6 +503,138 @@ showlinks:
   RTS
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; dolinks
+;; Process input for a list of links, watching for keyboard and mouse input.
+;; Loops until link is pressed.
+;; 
+;; LINKPTR contains first link of a page, with format as described below.
+dolinks:
+
+@dolinks_loop:
+  BIT PBTN0 ; Read paddle 0 button.
+  BPL @dolinks_key ; Button not pressed, jump to keyboard processing.
+  LDX #1    ; Read paddle 1 (y-position)
+  JSR PREAD ; Read paddle position into y.
+  TYA ; Transfer paddle position to A.
+  TAX ; Transfer paddle position to X.
+
+  LDY #0
+@dolinks_check:
+  LDA (LINKPTR),Y
+  BEQ @dolinks_loop ; Done with all links, start over.
+
+  INY ; Index of link lower y-position
+  TXA ; Paddle position into A
+  CMP (LINKPTR),Y
+  BCC @no_hit
+
+  INY ; Index of link upper y-position
+  TXA ; Paddle position into A
+  CMP (LINKPTR),Y
+  BCS @no_hit
+
+  ; Now we know we are on the link.
+  ; Store jump address in LINKJUMP.
+  INY
+  LDA (LINKPTR),Y
+  STA LINKJUMP
+  INY
+  LDA (LINKPTR),Y
+  STA LINKJUMP+1
+  JMP (LINKJUMP)
+
+@no_hit:
+  ; Skip over link text.
+  INY
+  LDA (LINKPTR),Y
+  BNE @no_hit
+  ; Now we are at the next link.
+  JMP @dolinks_check
+
+@dolinks_key:
+  ; Check for keypress
+  LDA $C000
+  BPL @dolinks_loop ; If nothing pressed.
+  STA $C010 ; Clear keyboard strobe
+
+  JSR PRBYTE ; Debugging
+  TAX ; Stash key in X
+  
+  LDY #0
+@dolinks_key_check:
+  LDA #'<'|$80
+  JSR COUT
+  TYA ; Debugging - Print Y index
+  JSR PRBYTE
+  LDA #'>'|$80
+  JSR COUT
+  TXA ; Debugging - print X val (key)
+  JSR PRBYTE
+  LDA (LINKPTR),Y
+  JSR PRBYTE ; Debugging - print index
+
+  LDA (LINKPTR),Y ; Get index
+  BEQ @dolinks_loop ; Done with all links, start over.
+
+  LDA (LINKPTR),Y ; Get index
+  JSR PRBYTE ; Debugging, print index again
+
+  TXA ; Get keypress into A
+  CMP (LINKPTR),Y   ; Does key match index?
+  BNE @no_key_hit
+
+  ; Key matched link.
+  LDA #'*'|$80 ; Debug
+  JSR COUT ; Debug
+
+  INY ; Skip paddle position indices
+  INY
+  ; Store jump address in LINKJUMP
+  INY
+  LDA (LINKPTR),Y
+  STA LINKJUMP
+  JSR PRBYTE
+  INY
+  LDA (LINKPTR),Y
+  STA LINKJUMP+1
+  JSR PRBYTE
+
+  LDA #'*'|$80 ; Debug
+  JSR COUT ; Debug
+  JMP (LINKJUMP)
+
+@no_key_hit:
+  LDA #$33   ; Debugging
+  JSR PRBYTE ; Debugging
+  INY ; Skip paddle position indices
+  INY
+  INY ; Skip jump word
+  INY
+@no_key_hit_loop:
+  ; Skip over link text.
+  INY
+
+  TYA ; Debug
+  JSR PRBYTE ; Debug
+  LDA (LINKPTR),Y ; Debug
+  JSR PRBYTE ; Debug
+
+  LDA (LINKPTR),Y
+  BNE @no_key_hit_loop
+
+  ; Now we are at the next link.
+  INY
+  LDA #'['|$80
+  JSR COUT
+  TYA
+  JSR PRBYTE
+  LDA #']'|$80
+  JSR COUT
+  LDA (LINKPTR),Y
+  JSR PRBYTE
+  JMP @dolinks_key_check
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Page data.
 
 ;; Individual link format:
@@ -626,21 +644,42 @@ showlinks:
 ;;   - work: Address to jsr when link clicked
 ;;   - asciiz: Link text
 
+main_menu_links:
+  .byte '1'|$80,$65,$6F
+  .word about_matt
+  .asciiz "  1. About Matt"
+  .byte '2'|$80,$7a,$84
+  .word pubs
+  .asciiz "  2. Research papers and talks"
+  .byte '3'|$80,$7a,$84
+  .word resume
+  .asciiz "  3. My resume and CV"
+  .byte '4'|$80,$7a,$84
+  .word about_website
+  .asciiz "  4. Enter the dungeon"
+  .byte '5'|$80,$7a,$84
+  .word about_website
+  .asciiz "  5. About this website"
+  .byte 0
+
 about_links:
-  .byte '1',$65,$6F
+  .byte '1'|$80,$65,$6F
   .word about_link_1_jump
-  .asciiz "github.com/mdwelsh/mdwos-6502"
-  .byte '2',$7a,$84
+  .asciiz "[1] github.com/mdwelsh/mdwos-6502"
+  .byte '2'|$80,$7a,$84
   .word about_link_2_jump
-  .asciiz "www.scullinsteel.com/apple2"
+  .asciiz "[2] www.scullinsteel.com/apple2"
+  .byte '3'|$80,$7a,$84
+  .word main_menu
+  .asciiz "[3] Go back"
   .byte 0
 
 about_link_1_jump:
   jsr doopenlink
   .asciiz "https://github.com/mdwelsh/mdwos-6502"
-  rts
+  jmp about_website
 
 about_link_2_jump:
   jsr doopenlink
   .asciiz "https://www.scullinsteel.com/apple2/"
-  rts
+  jmp about_website
